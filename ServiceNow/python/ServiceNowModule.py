edf81cc00a883c024552085e29c90e5a1139769d
Youcef <58149911+Youseii@users.noreply.github.com>
2025-08-29 12:06:58 +0200

    Update ServiceNowModule.py
    v1 Module Snow

--- ServiceNow/python/ServiceNowModule.py
+++ ServiceNow/python/ServiceNowModule.py
@@ -1,1 +1,30 @@
-aze
+import requests
+import json
+
+
+def credentials(credential_file):
+    """
+    Get the credentials from the json file
+    @return: credentials
+    @rtype: dict
+    """
+    with open(credential_file) as json_file:
+        data = json.load(json_file)
+
+    return data
+
+
+def auth_service_now(url_instance, credential_file):
+    """
+    Authentication to serviceNow
+    @return: Authentication token
+    @rtype: basestring
+    """
+
+    print("Beginning the auth to ServiceNow")
+    url = f'https://{url_instance}/oauth_token.do'
+    auth = credentials(credential_file)
+
+    x = requests.post(url, data=auth['credentials']["Oauth"])
+    data = x.json()
+    return data
